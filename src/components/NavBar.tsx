3f61fbb2c923fac03d6cade525299da0c5b8ade6
Israel Adebayo <isszzyraelite@gmail.com>
2025-06-22 16:32:44 +0100

    setup navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,6 +12,29 @@ const NavBar = () => {
         </a>
         <i className="bx bx-sun md:text-3xl sm:text-2xl text-xl text-gray-200 sm:ml-4 ml-2 cursor-pointer"></i>
       </div>
+      <div>
+        <a
+          href="#"
+          className="group lg:text-lg md:text-base text-sm font-light text-yellow-500 lg:mr-12 mr-8 trackling-wide relative"
+        >
+          Home
+          <span className="absolute -bottom-1 left-0 w-full h-[1px] bg-yellow-500 transform scale-x-0 group-hover:scale-x-100 group-hover:origin-bottom-left origin-right transition duration-300"></span>
+        </a>
+        <a
+          href="#"
+          className="group lg:text-lg md:text-base text-sm font-light text-white lg:mr-12 mr-8 trackling-wide relative"
+        >
+          Services
+          <span className="absolute -bottom-1 left-0 w-full h-[1px] bg-white transform scale-x-0 group-hover:scale-x-100 group-hover:origin-bottom-left origin-right transition duration-300"></span>
+        </a>
+        <a
+          href="#"
+          className="group lg:text-lg md:text-base text-sm font-light text-white lg:mr-12 mr-8 trackling-wide relative"
+        >
+          Contact
+          <span className="absolute -bottom-1 left-0 w-full h-[1px] bg-white transform scale-x-0 group-hover:scale-x-100 group-hover:origin-bottom-left origin-right transition duration-300"></span>
+        </a>
+      </div>
     </div>
   );
 };
